ac17d6257ba30213fbe6ed7ec216224c3ad01e6f
JMYOO <jmyoo213@gmail.com>
2026-05-09 01:16:42 +0900

    하나은행 배경색 #009490으로 변경

--- components/BankSelectModal.tsx
+++ components/BankSelectModal.tsx
@@ -24,7 +24,7 @@ const BANKS: Bank[] = [
   { name: "산업은행",         img: "/banks/kdb.png",   bg: "#0A1B98" },
   { name: "우리은행",         img: "/banks/woori.png", bg: "#C5E0F5", size: "w-9 h-9" },
   { name: "한국씨티은행",     img: "/banks/citi_new.jpg" },
-  { name: "하나은행",         img: "/banks/hana_new.png", bg: "#009B77", white: true },
+  { name: "하나은행",         img: "/banks/hana_new.png", bg: "#009490", white: true },
   { name: "SC제일은행",       img: "/banks/sc_logo.png", bg: "#1E6F5C" },
   { name: "경남은행",         svg: "/banks/039.svg",  bg: "#C8102E" },
   { name: "광주은행",         img: "/banks/gwangju.png" },
